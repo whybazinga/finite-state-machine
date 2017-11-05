bbddd9c99138ee0821ec8625439e355dd8c95c69
Alex Muharski <aom9@mail.ru>
2017-11-06 00:56:33 +0300

    half the way :]

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,55 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (config !== null) {
+            this.initial = config.initial;
+            this.current = this.initial;
+            this.states = config.states;;
+        } else {
+            throw new Error;
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.current;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (Object.keys(this.states).some((str) => str.includes(state))) {
+            this.current = state;
+        } else {
+            throw new Error;
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (Object.keys(this.states[this.current]["transitions"]).some((str) => str.includes(event))) {
+            this.current = this.states[this.current]["transitions"][event];
+        }
+        else {
+            throw new Error;
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.current = this.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +59,42 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (event === undefined) {
+            return Object.keys(this.states);
+        }
+        else if (Object.keys(this.states).some((temp) => Object.keys(this.states[temp]["transitions"]).some((str) => str.includes(event)))) {
+            let result = [];
+            for (let i = 0; i < Object.keys(this.states).length; i++) {
+                if (Object.keys(this.states[Object.keys(this.states)[i]]["transitions"]).some((str) => str.includes(event))) {
+                    result.push(Object.keys(this.states)[i]);
+                }
+            }
+            return result;
+        }
+        else {
+            return [];
+        }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() { }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() { }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() { }
 }
 
 module.exports = FSM;
